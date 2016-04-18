f96b5c7037d43a53ddaa2b6ce67b9f006da93ee4
mucamaca <bor.groseljsimic@telemach.net>
2016-04-18 18:59:58 +0200

    adding food eating support

--- snake/snake.ino
+++ snake/snake.ino
@@ -11,8 +11,8 @@ struct piece{
 struct snake_t{
   char x;
   char y;
-  char ldx;
-  char ldy;
+  signed char ldx;
+  signed char ldy;
   char index;
   char len;
   struct piece *arr;
@@ -21,6 +21,7 @@ struct snake_t{
 static char obstacle_len = 0;
 static struct snake_t *snake;
 static struct piece *obst;
+static struct piece food;
 
 
 void update_snake(){
@@ -31,7 +32,7 @@ void update_snake(){
   
 
 void move(){
-  char dx = 0,dy = 0;
+  signed char dx = 0,dy = 0;
   if(analogRead(5) - 512 < -100){
     dx = -1;
   }
@@ -66,7 +67,12 @@ void move(){
 }
 
 void collision(){};
-void eat(){};
+void eat(){
+  if(snake->x == food.x && snake->y == food.y){
+    snake->len++;
+    score++;
+  }
+}
 
 void update(){
   move();
@@ -93,29 +99,54 @@ void end_game(){
   }
   free(snake);
   free(obst);
-  //goto menu;
+  exit(0);
 }
 
 void render(){
   display.clear();
   draw_snake();
   display.drawPixel(snake->x, snake->y, WHITE);
+  display.drawPixel(food.x, food.y, WHITE);
   display.update();
 }
 
+void snake_init(){
+  snake->len = 5;
+  snake->arr = (struct piece *)malloc(sizeof(struct piece) * snake->len);
+  if(snake->arr == NULL)
+    exit(1);
+  snake->x = 50;
+  snake->y = 50;
+  snake->ldx = 0;
+  snake->ldy = -1;
+  snake->index = 0;
+}
+
 void setup(){
+  delay(1000);
+  Serial.begin(9600);
   display.initialize();
   snake=(struct snake_t *)malloc(sizeof(struct snake_t));
-  //snake_init(snake);
+  if(snake == NULL ){
+    Serial.println("you retard!");
+    exit(1); // to je temporary. drgac ne bo kr koncal programa zarad null pointerja
+  }
+  snake_init();
+
+  randomSeed(micros()+(int)analogRead(4));
+  food.x=42;
+  food.y=42;
+  //Serial.println("meow. im here");
   char i;
   for(i = 1; i < snake->len * 4; i += 4){
-    display.drawPixel(snake->x, snake->y + i, WHITE);
+    display.drawLine(snake->x, snake->y + i, snake->x, snake->y + i + 4, WHITE);
   }
 }
 
 void loop(){
-  
   update();
   render();
 }
+
+
 
